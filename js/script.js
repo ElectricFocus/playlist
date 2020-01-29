890a09512b6a91afface10cbf8d8b1bab2a4cf6f
ElectricFocus <Bobtherook@gmail.com>
2020-01-29 23:26:26 +0000

    hi

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let snn=[  ];
+let ann=[  ];
+let sll=[  ];
+let iuu=[  ];
+let slaa=[  ];
+
+$("#add").click(function () {
+    let addy = ("#sla").val();
+    let picture  = ("#iu").val();
+    let long  = ("#sl").val();
+    let sing = ("#sn").val();
+    let singer= ("#an").val();
+    sll.push(long);
+    snn.push(sing);
+    ann.push(singer);
+    iuu.push(picture);
+    slaa.push(addy);
+
+
+
+
+    snn.foreach(function(songx) {
+       $("#song").append(songx);
+    });
+
+    sll.foreach(function(like) {
+
+    
+    });
+    likes.foreach(function(like) {
+    
+    
+    });
+    likes.foreach(function(like) {
+    
+    });
+    likes.foreach(function(like) {
+    
+    
+    });
+    
+
+});
