7dda073bfc6121467985d39ebe9fe372c182a228
ElectricFocus <Bobtherook@gmail.com>
2020-02-05 23:23:12 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -1,43 +1,49 @@
-let snn=[  ];
-let ann=[  ];
-let sll=[  ];
-let iuu=[  ];
-let slaa=[  ];
+let snn = ["stupid"];
+let ann = ["Nascar aloe"];
+let sll = ["3mins"];
+let iuu = ["https://images.genius.com/f865b7d8315eeb7d2d380bf32b7f170a.300x300x1.jpg"];
+let slaa = ["https://www.youtube.com/watch?v=9zdjnEmMQh0"];
 
 $("#add").click(function () {
-    let addy = ("#sla").val();
-    let picture  = ("#iu").val();
-    let long  = ("#sl").val();
-    let sing = ("#sn").val();
-    let singer= ("#an").val();
+    let addy = $("#sla").val();
+    let picture = $("#iu").val();
+    let long = $("#sl").val();
+    let sing = $("#sn").val();
+    let singer = $("#an").val();
     sll.push(long);
     snn.push(sing);
     ann.push(singer);
     iuu.push(picture);
     slaa.push(addy);
 
+    $(".song").empty();
+    $(".artist").empty();
+    $(".slength").empty();
+    $(".iurl").empty();
+    $(".sladdress").empty();
 
 
 
-    snn.foreach(function(songx) {
-       $("#song").append(songx);
+
+
+
+    snn.forEach(function (songx) {
+        $(".song").append(songx);
     });
 
-    sll.foreach(function(like) {
+    sll.forEach(function (songL) {
+        $(".slength").append(songL);
+    });
+    ann.forEach(function (artistN) {
+        $(".artist").append(artistN);
+    });
+    iuu.forEach(function (imageU) {
+        $(".iurl").append(imageU);
+    });
+    slaa.forEach(function (songlink) {
+        $(".sladdress").append(songlink);
+    });
 
-    
-    });
-    likes.foreach(function(like) {
-    
-    
-    });
-    likes.foreach(function(like) {
-    
-    });
-    likes.foreach(function(like) {
-    
-    
-    });
-    
-
+    console.log("hi");
 });
+// "<" + "" + ">"
